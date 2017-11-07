5868c0e2ab800cf1a3db6d92e1b1d0da9356154e
IlyaMan <kelim.mail@gmail.com>
2017-11-07 21:23:48 +0300

    SORTS

---
+++ bbl.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <limits.h>
+
+void bbl(unsigned int *arr, int n) {
+        unsigned int i;
+        unsigned int j;
+        unsigned int tmp;
+
+        for (i = 0; i < n; i++) {
+                for (j = 1; j < n; j++) {
+                        if (arr[j] < arr[j - 1]) {
+                                tmp = arr[j];
+                                arr[j] = arr[j - 1];
+                                arr[j - 1] = tmp;
+                        }
+                }
+        }
+}
+
+
+unsigned int main() {
+        unsigned int n = 100;
+        unsigned int arr[n];
+        for (unsigned int i = 0; i < n; i++) {
+                arr[i] = n - i;
+        }
+
+        bbl(arr, n);
+
+        for (unsigned int i = 0; i < n; i++) {
+                printf("%d ", arr[i]);
+        }
+
+        return 0;
+}
