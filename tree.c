88aee386cae4264c666aa19714676ada62092603
Elijah <Elijah@MacBook-Pro-Ila.local>
2017-12-26 21:21:14 +0300

    Tree

--- tree.c
+++ tree.c
@@ -36,13 +36,13 @@ void findix(struct node arr[], int n){
 }
 
 int findRoot(struct node arr[], int l, int r){
+        printf("finding root between %d AND %d\n", l, r);
         if (r == l) {
                 return r;
         }
         int min = l;
         int valmin = abs(arr[l + 1].x - arr[l].x - arr[r].x);
         int tmp;
-        printf("SATRVAl: %d STARTMP: %d\n", arr[min].val, valmin);
         for (int i = l; i <= r; i++) {
                 int left, right;
                 if (i == l) {
@@ -59,22 +59,56 @@ int findRoot(struct node arr[], int l, int r){
                         right = arr[i+1].x;
                 }
                 tmp = abs(left + right - arr[l].x - arr[r].x);
-                printf("VAl: %d TMP: %d\n", arr[i].val, tmp);
                 if (tmp < valmin) {
                         min = i;
                         valmin = tmp;
                 }
+        }
+        printf("root is %d = %d\n", min, arr[min].val);
+        if (min > l){
+          printf("%d GOING LEFT\n", arr[min].val);
+          arr[min].l = findRoot(arr, l, min - 1);
+          printf("%d L> %d\n", arr[min].val, arr[arr[min].l].val );
+        }
+        if (min < r){
+          printf("%d GOING RIGHT\n", arr[min].val);
+          arr[min].r = findRoot(arr, min + 1, r);
+          printf("%d R> %d\n", arr[min].val, arr[arr[min].r].val );
 
         }
         return min;
 
 }
 
+// int tree(struct node arr[], int l, int r){
+//         int ro = findRoot(arr, l, r);
+//         if (ro != l) {
+//                 arr[r].l = tree(arr, l, ro - 1);
+//         }
+//         if (ro != r) {
+//                 arr[r].r = tree(arr, ro + 1, r);
+//         }
+//         // printf("TREE log val: %d, l: %d, r: %d\n", arr[ro].val, arr[ro].l, arr[ro].r);
+//         return ro;
+// }
 
-void buildTree(struct node arr[], int n){
-  int root = findRoot(arr, 0, n-1);
-  arr 
+
+void graviz(struct node arr[], int n){
+        FILE *fp;
+        fp = fopen("fo.txt", "w");
+        fprintf(fp, "%s", "digraph {\n");
+        for (int i = 0; i < n; i++) {
+                if ((arr[i].l < n)) {
+                        fprintf(fp, "\"%d\\n%d\" -> \"%d\\n%d\";\n", arr[i].val, arr[i].freq, arr[arr[i].l].val, arr[arr[i].l].freq);
+                }
+                if ((arr[i].r < n)) {
+                        fprintf(fp, "\"%d\\n%d\" -> \"%d\\n%d\";\n", arr[i].val, arr[i].freq, arr[arr[i].r].val, arr[arr[i].r].freq);
+                }
+        }
+        fprintf(fp, "%s", "}");
+
 }
+
 
 int main(void){
         FILE *fp;
@@ -84,12 +118,16 @@ int main(void){
         struct node arr[n];
         for (int i = 0; i < n; i++) {
                 fscanf(fp, "%d - %d", &arr[i].val, &arr[i].freq);
+                arr[i].l = n + 10;
+                arr[i].r = n + 10;
         }
         structNaiveSort(arr, n);
         findix(arr, n);
-        for (int i = 0; i < n; i++) {
-                printf("val: %d, freq: %d, x: %d\n", arr[i].val, arr[i].freq, arr[i].x);
-        }
-        printf("%d", arr[findRoot(arr, 0, 2)].val);
+        // for (int i = 0; i < n; i++) {
+        //         printf("val: %d, freq: %d, x: %d\n", arr[i].val, arr[i].freq, arr[i].x);
+        // }
+        // tree(arr, 0, n - 1);
+        printf("%d\n", arr[findRoot(arr, 0, n - 1)].val);
+        graviz(arr, n);
         return 0;
 }
